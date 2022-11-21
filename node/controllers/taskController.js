f620a810c7fedf5c6f13dee7a5a7e4370873c483
Võ Kiên <94681725+changngoc29@users.noreply.github.com>
2022-11-21 13:36:38 +0700

    Delete node directory

--- node/controllers/taskController.js
+++
@@ -1,9 +0,0 @@
-exports.getAllTasks = (req, res) => {};
-
-exports.getTask = (req, res) => {};
-
-exports.createTask = (req, res) => {};
-
-exports.updateTask = (req, res) => {};
-
-exports.deleteTask = (req, res) => {};
--- node/routes/taskRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const taskController = require('./../controllers/taskController');
-
-const router = express.Router();
-
-router.route('/').get(taskController.getAllTasks);
-
-module.exports = router;
